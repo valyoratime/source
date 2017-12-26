501a675ad16a2ce30e54fae5093ad485590bcff5
valyoratime <billikotacom@gmail.com>
2017-12-27 01:29:16 +0300

    Basic implementation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import * as moment from 'moment-business-days'
+import { holidays } from './holidays'
+import DeclensionHelper from './DeclensionHelper'
+
+moment.locale('ru', {
+  holidays: holidays,
+  holidayFormat: 'DD-MM-YYYY',
+  workingWeekdays: [1, 2, 3, 4, 5],
+})
+
 
 class App extends Component {
+  newYear = () => {
+    return (new Date()).getFullYear() === 2017
+      ? moment('31-12-2017', 'DD-MM-YYYY')
+      : moment('31-12-2018', 'DD-MM-YYYY')
+  }
+  
+  now = () => moment(new Date())
+  dayOfWeek = () => this.now().day()
+  nowIsWeekend = () => this.dayOfWeek() === 0 || this.dayOfWeek() === 6
+  
+  daysLeftForm = (now = this.now()) => {
+    return Math.round(this.newYear().diff(now, 'days', true))
+  }
+  
+  businessDaysLeft = (to = this.newYear()) => {
+    const diff = Math.round(to.businessDiff(this.now(), true))
+    
+    if (this.nowIsWeekend()) {
+      return diff
+    } else {
+      return diff - 1
+    }
+  }
+  
+  daysWord = number => DeclensionHelper.daysWord(number)
+  workWord = number => DeclensionHelper.workWord(number)
+  leftWord = number => DeclensionHelper.leftWord(number)
+  
   render() {
+    const daysLeft = this.daysLeftForm()
+    const dayWord = this.daysWord(daysLeft)
+    const leftDayWord = this.leftWord(daysLeft)
+    
+    const workingDaysLeft = this.businessDaysLeft()
+    const workWord = this.workWord(workingDaysLeft)
+    const workingDayWord = this.daysWord(workingDaysLeft)
+    const leftWorkingDayWord = this.leftWord(workingDaysLeft)
+    
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Возрадуйся же, <span>@valyora</span></h1>
+        <div className="left">
+          <p className="normal-days">Ибо до нового года {leftDayWord} {daysLeft} {dayWord}!</p>
+          <p className="working-days">
+            А это значит, что {leftWorkingDayWord} {workingDaysLeft} {workWord} {workingDayWord}!
+          </p>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/DeclensionHelper.js
@@ -0,0 +1,72 @@
+class DeclensionHelper {
+  static daysWord = number => {
+    const lastDigit = DeclensionHelper.lastDigit(number)
+    
+    switch(lastDigit) {
+      case '0':
+        return 'дней'
+      case '1':
+        return 'день'
+      case '2':
+      case '3':
+      case '4':
+        return 'дня'
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        return 'дней'
+      default:
+        return 'чет сломалось'
+    }
+  }
+  
+  static workWord = number => {
+    const lastDigit = DeclensionHelper.lastDigit(number)
+    
+    switch(lastDigit) {
+      case '1':
+        return 'рабочий'
+      case '0':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        return 'рабочих'
+      default:
+        return 'чет сломалось'
+    }
+  }
+  
+  static leftWord = number => {
+    const lastDigit = DeclensionHelper.lastDigit(number)
+    
+    switch(lastDigit) {
+      case '1':
+        return 'остался'
+      case '0':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        return 'осталось'
+      default:
+        return 'чет сломалось'
+    }
+  }
+  
+  static lastDigit = number => {
+    return number.toString().split('').pop()
+  }
+}
+
+export default DeclensionHelper
